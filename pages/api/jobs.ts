fb584b6e2eab7702961abac9016d94d7f4075302
zackkrida <zackkrida@pm.me>
2020-06-04 14:00:10 -0400

    Add .env.example; Refactor three60 class a bit

---
+++ pages/api/jobs.ts
@@ -0,0 +1,19 @@
+import { connectTo360 } from 'lib/three60'
+
+export default async function Salesforce(_, res) {
+  try {
+    const t60 = await connectTo360()
+    // https://improveit360-3562.cloudforce.com/01I1I000001rrvm?setupid=CustomObjects
+
+    // prettier-ignore
+    const projects = await t60
+      .sobject('i360__Project__c')
+      .select(['*'])
+      .limit(5)
+
+    res.json(projects)
+  } catch (error) {
+    console.error(error)
+    res.json({ error: true, message: error.message })
+  }
+}
