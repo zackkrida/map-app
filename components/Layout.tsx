188e0508c697ecf632e5a85371d10c8027a3382a
zackkrida <zackkrida@pm.me>
2020-06-08 23:04:37 -0400

    Filter + search styling tweaks

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -46,9 +46,10 @@ export function Layout({ mapPos, mapChildren, children }: LayoutProps) {
     if ('searchTerm' in router.query) {
       setSearchTerm(router.query.searchTerm as string)
       setResultsLoading(true)
-      fetchMore({ searchTerm: router.query.searchTerm }).then(_ =>
-        setResultsLoading(false)
-      )
+      fetchMore({
+        searchTerm: router.query.searchTerm,
+        searchType: router.query.searchType,
+      }).then(_ => setResultsLoading(false))
     }
   }, [router.query])
 
@@ -121,79 +122,105 @@ export function Layout({ mapPos, mapChildren, children }: LayoutProps) {
         {/* Results Sidebar */}
         <div className="w-full md:max-w-md md:flex md:flex-col md:max-h-screen shadow-md z-20 md:m-4 md:rounded-md md:ml-auto overflow-hidden bg-white">
           <div className="pt-4 px-2 bg-brand-navy text-white shadow-md">
-            <form className="flex rounded-md shadow-sm" onSubmit={handleSubmit}>
-              <input
-                value={searchTerm}
-                onChange={event => setSearchTerm(event.target.value)}
-                className="form-input flex-1 block w-full rounded-none rounded-l-md transition duration-150 ease-in-out sm:text-sm sm:leading-5 placeholder-cool-gray-500 text-brand-navy"
-                placeholder={`Search projects by ${searchType}`}
-              />
-              <button
-                disabled={searchTerm.length === 0 || resultsLoading}
-                type="submit"
-                className="appearance-none inline-flex items-center px-3 rounded-r-md border border-l-0 border-blue-500 bg-brand-blue text-white text-sm active:bg-blue-500"
-              >
-                {!resultsLoading ? (
-                  <svg
-                    fill="currentColor"
-                    className="w-6 h-6"
-                    viewBox="0 0 20 20"
+            <form className="" onSubmit={handleSubmit}>
+              <div className="flex md:rounded-md shadow-sm relative rounded-none rounded-l-md transition duration-150 ease-in-out sm:text-sm sm:leading-5  text-brand-navy">
+                <input
+                  value={searchTerm}
+                  onChange={event => setSearchTerm(event.target.value)}
+                  className="flex-1 block w-full form-input pr-28 placeholder-cool-gray-500"
+                  placeholder={`Search projects by ${searchType}`}
+                />
+                <div className="absolute inset-y-0 right-0 flex items-center">
+                  <select
+                    aria-label="Search Type"
+                    value={searchType}
+                    onChange={event =>
+                      setSearchType(event.target.value as SearchTypes)
+                    }
+                    className="form-select h-full py-0 pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm sm:leading-5 rounded-r-md rounded-l-none bg-blue-100 border-l border-blue-200"
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                ) : (
-                  <svg
-                    className="w-6 h-6"
-                    viewBox="0 0 100 100"
-                    preserveAspectRatio="xMidYMid"
-                  >
-                    <g>
-                      <path
-                        d="M50 15A35 35 0 1 0 74.74873734152916 25.25126265847084"
-                        fill="none"
-                        stroke="#ffffff"
-                        stroke-width="12"
-                      ></path>
-                      <path d="M49 3L49 27L61 15L49 3" fill="#ffffff"></path>
-                      <animateTransform
-                        attributeName="transform"
-                        type="rotate"
-                        repeatCount="indefinite"
-                        dur="1s"
-                        values="0 50 50;360 50 50"
-                        keyTimes="0;1"
-                      ></animateTransform>
-                    </g>
-                  </svg>
-                )}
-              </button>
-            </form>
+                    {Object.entries(SearchTypes).map(([key, value]) => (
+                      <option key={key} value={value}>
+                        {key}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+              </div>
 
-            <div className="pt-4 pb-2 flex justify-between items-center">
-              <p className="text">
-                Showing <strong>{projects.length}</strong> Projects
-              </p>
-              <p>
-                <button className="bg-white bg-opacity-25 px-2 py-1 rounded-md inline-flex border-white border-2 border-opacity-0 items-center text-sm focus:outline-none focus:border-opacity-100 focus:shadow-md focus:bg-opacity-100 focus:text-brand-blue focus:outline-none hover:border-opacity-100 hover:shadow-md hover:bg-opacity-100 hover:text-brand-blue transition-bg duration-100 ease-in-out">
-                  <svg
-                    className="w-4 h-4 mr-2"
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+              <div className="pt-2 pb-2 flex justify-between items-center">
+                <p className="text-xs md:text-sm">
+                  <strong>{projects.length}</strong> Results
+                </p>
+
+                <div className="flex items-center">
+                  <div className="mr-2 w-20">
+                    <select
+                      defaultValue={null}
+                      id="country"
+                      className="text-gray-500 mr-2 block form-select w-full py-2 px-2 pr-6 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5 truncate"
+                    >
+                      <option value={null}>Year</option>
+                      {new Array(30).fill('').map((_, i) => (
+                        <option value={new Date().getFullYear() - i}>
+                          {new Date().getFullYear() - i}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+
+                  <button
+                    disabled={searchTerm.length === 0 || resultsLoading}
+                    type="submit"
+                    className="appearance-none inline-flex items-center px-2 py-2 rounded-md border border-l-0 border-blue-500 bg-brand-blue text-white text-sm active:bg-blue-500"
                   >
-                    <path d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"></path>
-                  </svg>
-                  Filter Results
-                </button>
-              </p>
-            </div>
+                    {!resultsLoading ? (
+                      <>
+                        <span className="mr-1">Search</span>
+                        <svg
+                          fill="currentColor"
+                          className="w-5 h-5"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                            clipRule="evenodd"
+                          ></path>
+                        </svg>
+                      </>
+                    ) : (
+                      <svg
+                        className="w-5 h-5"
+                        viewBox="0 0 100 100"
+                        preserveAspectRatio="xMidYMid"
+                      >
+                        <g>
+                          <path
+                            d="M50 15A35 35 0 1 0 74.74873734152916 25.25126265847084"
+                            fill="none"
+                            stroke="#ffffff"
+                            stroke-width="12"
+                          ></path>
+                          <path
+                            d="M49 3L49 27L61 15L49 3"
+                            fill="#ffffff"
+                          ></path>
+                          <animateTransform
+                            attributeName="transform"
+                            type="rotate"
+                            repeatCount="indefinite"
+                            dur="1s"
+                            values="0 50 50;360 50 50"
+                            keyTimes="0;1"
+                          ></animateTransform>
+                        </g>
+                      </svg>
+                    )}
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
 
           {/* <div className="absolute top-0 left-0 max-h-screen overflow-scroll bg-white text-sm max-w-2xl">
